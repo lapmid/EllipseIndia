3cc36727ac1ff6b8a18353b266c7c8be86e6b3e7
Anurag Pandey <lapmid258@gmail.com>
2019-01-21 15:13:41 +0530

    added contact info

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -141,3 +141,21 @@ $("#hidden-iframe").attr("src", "http://ellipseenterprise.com/");
 			// 	}
 			// }
 				
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navigation-bar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
